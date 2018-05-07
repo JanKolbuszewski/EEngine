198ba9be577859806ab3d3d727cbe133e9154a16
JanKolbuszewski <37754429+JanKolbuszewski@users.noreply.github.com>
2018-05-07 17:51:03 +0200

    v5

--- src/core/GameObject.java
+++ src/core/GameObject.java
@@ -3,39 +3,77 @@ package core;
 import java.util.ArrayList;
 
 public class GameObject {
-String name; //enum mType {Pane,Block,Ball}; //typy obiekotw do dodania
-int mID;
-boolean mGameState;
-static int id_source = 997;
-int mComponents = 0;
-long mLifeTime = 9999999;
-long mLifeStart = System.nanoTime();
-
-
-long mTimeLived() {
-	long time = mLifeStart - System.nanoTime();
-	return time;
-}
-
-public GameObject(long life_time,String name )
-{
-	mLifeTime = life_time;
-	mID = id_source;
-	id_source ++;
-	this.name = name;
-}
-
-void kill() {
-	//destroy all components
-}
-
-void update() 
-{
+	//id
+	String name;
+	int mID;
+	static int id_source = 997;
+	//lifetime
+	long mLifeTime = 9999999;
+	long mLifeStart = System.nanoTime();
+	//Components
+	boolean isPhysicsComponent  = false;
+	boolean isInputComponent  = false;
+	boolean isSoundComponent  = false;
+	boolean isGraphicComponent  = false;
 	
-	if(mLifeTime<mTimeLived()) {
-		kill();
-	}else{
-		//update component list
+	long mTimeLived() {
+		long time = mLifeStart - System.nanoTime();
+		return time;
 	}
-}
+	
+	public GameObject(long life_time,String name)
+	{
+		mLifeTime = life_time;
+		mID = id_source;
+		id_source ++;
+		this.name = name;
+	}
+	
+	void kill() 
+	{
+		//kill
+	}
+	//updating objects
+	void update() 
+	{
+		
+		if(mLifeTime<mTimeLived()) 
+		{
+			kill();
+		}
+		else if(Process.mKill)
+		{
+			kill();
+		}
+		else if(isPhysicsComponent) 
+		{
+			PhysicsComponent.update();
+		}
+		else if(isInputComponent) 
+		{
+			inputComponent.update();
+		}
+		else if(isSoundComponent) 
+		{
+			addSoundComponent.update();
+		}
+		else if(isGraphicComponent) 
+		{
+			addGraphicComponent.update();
+		}
+	}
+	//adding components
+	void addPhysicsComponent() {
+		
+	}
+	void addInputComponent() {
+		
+	}
+	void addSoundComponent() {
+		
+	}
+	void addGraphicComponent() {
+		
+	}
+
 }
--- src/core/Process.java
+++ src/core/Process.java
@@ -2,46 +2,42 @@ package core;
 
 import java.util.ArrayList;
 
+import xd.GameObject;
+import xd.Main;
+
 public class Process {
-boolean mKill = false;
-boolean mActive = false;
-boolean mPaused = false;
-boolean mIsAttached = false;
-boolean mIsInitialised = false;
-int mNext = 987654321;
-
-static ArrayList<GameObject> ObjectList = new ArrayList<GameObject>();
-
-void update()
-{
+	static boolean mKill = false;
+	boolean mActive = false;
+	boolean mPaused = false;
+	boolean mIsAttached = false;
+	boolean mIsInitialised = false;
+	int mNext = 987654321;
 	
-	for(1) 
+	// object list
+	static ArrayList<GameObject> ObjectList = new ArrayList<GameObject>();
+	// updating array list
+	void update()
 	{
-		
-		
+		for(GameObject G : ObjectList)
+		{
+			G.update();
+		}
+	}
+	//kill all objects on list
+	void kill()
+	{
+		for(GameObject G : ObjectList)
+		{
+			G.kill();
+		}
 	}
 	
-}
-static GameObject setGameObject(long life_time,String name)
-{
-	
-	
-	GameObject object = new GameObject(life_time,name);
-	ObjectList.add(object);
-	
-	return object;
-}
-
-public void xd() {
-	GameObject ball = Process.setGameObject(10000,"xd");
-	
-	
-	
-}
-
-void kill()
-{
-	
-}
+	// create object ex: (creting ball object living 100sec) "GameObject ball = Main.setGameObject(100000,"ball");"
+	static GameObject setGameObject(long life_time,String name)
+	{
+		GameObject object = new GameObject(life_time,name);
+		ObjectList.add(object);
+		return object;
+	}
 
 }
